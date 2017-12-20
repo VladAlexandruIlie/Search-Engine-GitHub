396a803f73649574cc0b6e4408bb525bc906f21d
Vlad Alexandru Ilie <ilievladalexandru@gmail.com>
2017-12-20 13:50:24 +0100

    SCORES
    MORE SCORES

--- src/BM25Score.java
+++ src/BM25Score.java
@@ -2,6 +2,7 @@ public class BM25Score implements Score {
     private static float BM25Score;
     private static float TFScore;
     private static float IDFScore;
+    private static float TFIDFScore;
 
 
 
@@ -10,6 +11,7 @@ public class BM25Score implements Score {
         IDFScore idf = new IDFScore();
         TFScore =  tf.getTFScore(a,w) ;
         IDFScore =  idf.getIDFScore(a, index);
+        TFIDFScore = TFScore * IDFScore;
 
         int nrOfWords = w.getWords().size(), sum = 0, nr = 0;
         double c, average, k = 1.75, b = 0.75;
@@ -22,6 +24,9 @@ public class BM25Score implements Score {
         average = (sum * 1.0) / nr;
         c = (TFScore * ((((k + 1) / k) * ((1 - b) + ((b * nrOfWords) / average))) + TFScore)) * IDFScore;
 
+        System.out.printf("(TF Score) %.4f * (IDF Score) %.4f  =  (TFIDF Score) %.4f and some magic = (BM25 Score) %.4f  on website: %s \n",
+                TFScore, IDFScore, TFIDFScore, BM25Score, w.getUrl());
+        
         BM25Score = (float) c;
         return BM25Score;
     }
